ddae30eae2e1fa52b24f86b90c4f4eb11b726387
Thomas <contact@thomas-george.com>
2021-05-09 20:57:40 +0200

    Initial Commit

---
+++ Guess_The_Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess_The_Flag
+//
+//  Created by Thomas George on 09/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess_The_Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess_The_Flag
+//
+//  Created by Thomas George on 09/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
