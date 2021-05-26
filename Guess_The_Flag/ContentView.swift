912104f74bebd98fa0f6819a04a86d0a4238de6e
Thomas George <thomasgeorge@mac-mini-de-thomas.home>
2021-05-26 19:09:05 +0200

    add animations when the flag choice is correct

--- Guess_The_Flag/ContentView.swift
+++ Guess_The_Flag/ContentView.swift
@@ -15,6 +15,9 @@ struct ContentView: View {
     @State private var showingAlert: Bool = false
     @State private var alertMessage: String = ""
     @State private var score: Int = 0
+    @State private var isCorrect: Bool = false
+    @State private var isBlured: Bool = false
+    @State private var selectedFlag: Int = 0
     
     var body: some View {
         ZStack {
@@ -31,13 +34,19 @@ struct ContentView: View {
                 
                 ForEach(0..<3){ number in
                     Button(action: {
-                        flagTapped(number)
+                        withAnimation {
+                            flagTapped(number)
+                        }
                     }) {
                         Image(flags[number])
                             .renderingMode(.original)
                             .clipShape(Capsule())
                             .overlay(Capsule().stroke(Color.white, lineWidth: 1))
+                            .opacity(self.isCorrect && self.selectedFlag != number ? 0.25 : 1)
+                            .scaleEffect(self.isCorrect && self.selectedFlag != number ? 0.75 : 1)
                     }
+                    .rotation3DEffect(.degrees(self.isCorrect && self.selectedFlag == number ? 360 : 0), axis: (x:0, y:1, z:0))
+                    
                 }
                 
                 VStack {
@@ -48,28 +57,38 @@ struct ContentView: View {
         }
         .alert(isPresented: $showingAlert) {
             Alert(title: Text(result), message: Text(alertMessage), dismissButton: .default(Text("Continue")) {
-                self.askQuestion()
+                    self.askQuestion()
             })
         }
     }
     
     func flagTapped(_ number: Int) {
+        self.selectedFlag = number
+        
         if flagsSelection == number {
-            result = "Correct"
-            alertMessage = "10 points"
-            score += 10
+            self.isCorrect = true
+            self.result = "Correct"
+            self.alertMessage = "10 points"
+            self.score += 10
         } else {
-            result = "Failed"
-            alertMessage = "Retry"
-            score = 0
+            self.result = "Failed"
+            self.alertMessage = "Retry"
+            self.score = 0
         }
-        showingAlert = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.showingAlert = true
+        }
     }
     
     func askQuestion() {
-        flags.shuffle()
-        flagsSelection = Int.random(in: 0...2)
-        showingAlert = false
+        self.isCorrect = false
+        self.flags.shuffle()
+        self.flagsSelection = Int.random(in: 0...2)
+        self.showingAlert = false
+    }
+    
+    func animateFlags(flag: Int) {
+        
     }
 }
 
